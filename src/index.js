6ab319c2058f8e0ddb92959ab394d888010cbf8e
danielvaldivv <dvaldiviesomerino.drvm@gmail.com>
2022-03-22 13:11:10 -0500

    Final Commit! <3

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 export { default as Button } from './Button'
 
-export { default as ContainterFlex } from './ContainterFlex'
+export { default as ContainerFlex } from './ContainerFlex'
 
-export { default as  MediaImg } from './ MediaImg'
+export { default as  MediaImg } from './MediaImg'
 
-export { default as  Text } from './ Text'
+export { default as  Text } from './Text'
