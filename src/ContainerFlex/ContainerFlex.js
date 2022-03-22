f601be8fa5e76fef6d418bf0268eae18ecc22fde
danielvaldivv <dvaldiviesomerino.drvm@gmail.com>
2022-03-22 15:16:22 -0500

    Added containerflex properties

--- src/ContainerFlex/ContainerFlex.js
+++ src/ContainerFlex/ContainerFlex.js
@@ -2,21 +2,33 @@ import { forwardRef } from 'react';
 import { cx, css } from '@emotion/css'
 import { PropTypes } from 'prop-types';
 
-const ContainerFlexStyles = (width, height, bgColor, flexDirection) => css`
+const ContainerFlexStyles = (width, height, bgColor, flexDirection,  alignContent, justifyContent, alignItems) => css`
   background-color: ${bgColor};
   width: ${width || '100%' };
   height: ${height || '100%'};
   display: flex;
   flex-direction: ${flexDirection};
+  align-content: ${alignContent } ;
+  justify-content: ${justifyContent} ;
+  align-items: ${alignContent };
 `
 
 const ContainerFlex = forwardRef((props, ref) => {
-  const { className, width, height, bgColor, flexDirection, ...otherProps } = props;
+  const {
+    className,
+    width,
+    height,
+    bgColor,
+    flexDirection,
+    alignContent,
+    justifyContent, 
+    alignItems,
+    ...otherProps } = props;
   return (
     <section
       ref={ref}
       {...otherProps}
-      className={cx(ContainerFlexStyles(width, height, bgColor, flexDirection), className)}
+      className={cx(ContainerFlexStyles(width, height, bgColor, flexDirection, alignContent, justifyContent, alignItems), className)}
     />
   );
 });
@@ -26,7 +38,10 @@ ContainerFlex.propTypes = {
   height: PropTypes.string,
   bgColor: PropTypes.string,
   className: PropTypes.string,
-  flexDirection: PropTypes.oneOf(['column', 'column-reverse', 'row', 'row-reverse'])
+  flexDirection: PropTypes.oneOf(['column', 'column-reverse', 'row', 'row-reverse']),
+  alignContent: PropTypes.oneOf(['center', 'flex-start', 'flex-end', 'space-around', 'space-between', 'stretch']),
+  justifyContent: PropTypes.oneOf(['center', 'flex-start', 'flex-end', 'space-around', 'space-between', 'space-evenly']),
+  alignItems: PropTypes.oneOf(['center', 'flex-start', 'flex-end', 'stretch']),
 };
 
 export default ContainerFlex;
